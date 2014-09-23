48d69ee3c2d434ef83962a49b52a308301119a7e
Ben Firshman <ben@firshman.co.uk>
2014-09-22 18:03:42 -0700

    Vendor digital ocean dependencies

--- hack/vendor.sh
+++ hack/vendor.sh
@@ -61,6 +61,8 @@ clone hg code.google.com/p/goauth2 afe77d958c70
 
 clone git github.com/MSOpenTech/azure-sdk-for-go 99078356867da23aa17bcbb0935ea06db9315fc5
 
+clone hg code.google.com/p/goauth2 afe77d958c70
+
 # get Go tip's archive/tar, for xattr support and improved performance
 # TODO after Go 1.4 drops, bump our minimum supported version and drop this vendored dep
 if [ "$1" = '--go' ]; then
