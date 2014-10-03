6044dd3f9a70bdff753d9ce8f566131612d6e153
Ruslan Gabitov <hope2believein@gmail.com>
2014-10-03 14:49:10 -0700

    Added azure-sdk dep & Fixed bug with missing VM
    Added azure-sdk dependency to hack\vendor.sh
    Fixed bug with missing VM in drivers\azure

--- hack/vendor.sh
+++ hack/vendor.sh
@@ -59,6 +59,8 @@ clone hg code.google.com/p/gosqlite 74691fb6f837
 
 clone hg code.google.com/p/goauth2 afe77d958c70
 
+clone git github.com/MSOpenTech/azure-sdk-for-go a870bf1573093df767a33e93d1d3a48a8b44f3e6
+
 # get Go tip's archive/tar, for xattr support and improved performance
 # TODO after Go 1.4 drops, bump our minimum supported version and drop this vendored dep
 if [ "$1" = '--go' ]; then
