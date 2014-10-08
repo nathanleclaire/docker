0b639394b7897073893089521bb714bf0e7de477
Ben Firshman <ben@firshman.co.uk>
2014-10-08 11:40:00 +0100

    Vendor azure-sdk-for-go

--- hack/vendor.sh
+++ hack/vendor.sh
@@ -59,7 +59,7 @@ clone hg code.google.com/p/gosqlite 74691fb6f837
 
 clone hg code.google.com/p/goauth2 afe77d958c70
 
-clone git github.com/MSOpenTech/azure-sdk-for-go v1.0
+clone git github.com/MSOpenTech/azure-sdk-for-go 99078356867da23aa17bcbb0935ea06db9315fc5
 
 # get Go tip's archive/tar, for xattr support and improved performance
 # TODO after Go 1.4 drops, bump our minimum supported version and drop this vendored dep
