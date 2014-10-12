29fcd6baa6482b849959accb1ee1d65d531504e1
Nathan LeClaire <nathan.leclaire@gmail.com>
2014-10-11 21:43:28 -0700

    Make instance Remove work!

--- hosts/drivers/aws/ec2/ec2.go
+++ hosts/drivers/aws/ec2/ec2.go
@@ -15,15 +15,17 @@ import (
 	"github.com/docker/docker/hosts/state"
 	"github.com/docker/docker/pkg/log"
 	flag "github.com/docker/docker/pkg/mflag"
+	"github.com/docker/docker/utils"
 	awsauth "github.com/smartystreets/go-aws-auth"
 )
 
 type Driver struct {
-	auth         aws.Auth
-	endpoint     string
+	Auth         aws.Auth
+	Endpoint     string
 	AccessKey    string
 	ImageId      string
 	InstanceId   string
+	InstanceName string
 	InstanceType string
 	IPAddress    string
 	Region       string
@@ -36,7 +38,9 @@ type CreateFlags struct {
 	AccessKey    *string
 	SecretKey    *string
 	ImageId      *string
+	InstanceName *string
 	Region       *string
+	Username     *string
 	InstanceType *string
 }
 
@@ -77,6 +81,11 @@ func RegisterCreateFlags(cmd *flag.FlagSet) interface{} {
 		DEFAULT_IMAGE_ID,
 		"AMI to use for the selected region",
 	)
+	createFlags.InstanceType = cmd.String(
+		[]string{"-aws-instance-name"},
+		"",
+		"Name of created instance",
+	)
 	createFlags.Region = cmd.String(
 		[]string{"-aws-region"},
 		DEFAULT_REGION,
@@ -87,7 +96,7 @@ func RegisterCreateFlags(cmd *flag.FlagSet) interface{} {
 		DEFAULT_INSTANCE_TYPE,
 		"Type of instance to create",
 	)
-	createFlags.InstanceType = cmd.String(
+	createFlags.Username = cmd.String(
 		[]string{"-aws-instance-username"},
 		DEFAULT_SSH_USERNAME,
 		"Username for SSH on the instance (depends on AMI)",
@@ -110,7 +119,7 @@ func (d *Driver) SetConfigFromFlags(flagsInterface interface{}) error {
 	d.ImageId = *flags.ImageId
 	d.Region = *flags.Region
 	d.InstanceType = *flags.InstanceType
-	d.endpoint = fmt.Sprintf("https://ec2.%s.amazonaws.com", d.Region)
+	d.Endpoint = fmt.Sprintf("https://ec2.%s.amazonaws.com", d.Region)
 
 	if (d.Region == DEFAULT_REGION) != (d.ImageId == DEFAULT_IMAGE_ID) {
 		return fmt.Errorf("Setting --aws-region without setting --aws-ami is disallowed")
@@ -118,13 +127,13 @@ func (d *Driver) SetConfigFromFlags(flagsInterface interface{}) error {
 
 	if d.AccessKey == "" || d.SecretKey == "" {
 		var err error
-		d.auth, err = aws.EnvAuth()
+		d.Auth, err = aws.EnvAuth()
 		if err != nil {
 			return fmt.Errorf("Setting the AWS_ACCESS_TOKEN and AWS_SECRET_KEY environment variables or the --aws-access-token and --aws-secret-key flags")
 		}
 	} else {
-		d.auth.AccessKey = *flags.AccessKey
-		d.auth.SecretKey = *flags.SecretKey
+		d.Auth.AccessKey = *flags.AccessKey
+		d.Auth.SecretKey = *flags.SecretKey
 	}
 
 	return nil
@@ -139,10 +148,14 @@ func (d *Driver) GetIP() (string, error) {
 }
 
 func (d *Driver) GetState() (state.State, error) {
+	v := url.Values{}
+	v.Set("Action", "DescribeInstances")
+	v.Set("InstanceId.1", d.InstanceName)
 	return state.Stopped, nil
 }
 
 func (d *Driver) Create() error {
+	d.setInstanceNameIfNotSet()
 	log.Infof("Creating AWS EC2 instance...")
 	instance, err := d.runInstance()
 	if err != nil {
@@ -152,10 +165,16 @@ func (d *Driver) Create() error {
 	return nil
 }
 
+func (d *Driver) setInstanceNameIfNotSet() {
+	if d.InstanceName == "" {
+		d.InstanceName = fmt.Sprintf("docker-host-%s", utils.GenerateRandomID())
+	}
+}
+
 func (d *Driver) makeAwsApiCall(v url.Values) (http.Response, error) {
 	v.Set("Version", "2014-06-15")
 	client := &http.Client{}
-	finalEndpoint := fmt.Sprintf("%s?%s", d.endpoint, v.Encode())
+	finalEndpoint := fmt.Sprintf("%s?%s", d.Endpoint, v.Encode())
 	req, err := http.NewRequest("GET", finalEndpoint, nil)
 	if err != nil {
 		return http.Response{}, fmt.Errorf("Error creating request from client")
@@ -166,10 +185,17 @@ func (d *Driver) makeAwsApiCall(v url.Values) (http.Response, error) {
 		SecretAccessKey: d.SecretKey,
 	})
 	resp, err := client.Do(req)
+	if err != nil {
+		return http.Response{}, fmt.Errorf("Client encountered error while doing the request: %s", err)
+	}
 	if resp.StatusCode != http.StatusOK {
 		return http.Response{}, fmt.Errorf("Non-200 API Response : \n%s", resp.StatusCode)
 	}
 	return *resp, nil
+}
+
+func NewApiCallError(err error) error {
+	return fmt.Errorf("Problem with AWS API call: %s", err)
 }
 
 func (d *Driver) runInstance() (Instance, error) {
@@ -183,7 +209,7 @@ func (d *Driver) runInstance() (Instance, error) {
 	resp, err := d.makeAwsApiCall(v)
 	defer resp.Body.Close()
 	if err != nil {
-		return instance, fmt.Errorf("Problem with AWS API call: %s", err)
+		return instance, NewApiCallError(err)
 	}
 	contents, err := ioutil.ReadAll(resp.Body)
 	if err != nil {
@@ -200,6 +226,15 @@ func (d *Driver) runInstance() (Instance, error) {
 }
 
 func (d *Driver) Remove() error {
+	log.Infof("Removing AWS EC2 instance...")
+	v := url.Values{}
+	v.Set("Action", "TerminateInstances")
+	v.Set("InstanceId.1", d.InstanceId)
+	resp, err := d.makeAwsApiCall(v)
+	if err != nil {
+		return NewApiCallError(err)
+	}
+	defer resp.Body.Close()
 	return nil
 }
 
