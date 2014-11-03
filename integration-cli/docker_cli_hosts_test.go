facc1103001b2ac52718fbbea9a10e45e596ebfe
Ben Firshman <ben@firshman.co.uk>
2014-11-03 16:17:14 +0000

    Add more integration tests

--- integration-cli/docker_cli_hosts_test.go
+++ integration-cli/docker_cli_hosts_test.go
@@ -3,29 +3,66 @@ package main
 import (
 	"fmt"
 	"net"
+	"os"
 	"os/exec"
+	"path"
 	"strings"
 	"testing"
 	"time"
+
+	homedir "github.com/mitchellh/go-homedir"
 )
 
 func TestHostsEnsureHostIsListed(t *testing.T) {
+	if err := clearHosts(); err != nil {
+		t.Fatal(err)
+	}
+
 	createCmd := exec.Command(dockerBinary,
 		"hosts",
 		"create",
-		"-o", "url=tcp://10.11.12.13:2375",
+		"--url", "tcp://10.11.12.13:2375",
 		"test")
 	out, _, err := runCommandWithOutput(createCmd)
-	errorOut(err, t, fmt.Sprintf("creating host failed with errors: %v", err))
+	if err != nil {
+		t.Fatal(out, err)
+	}
 
 	hostsCmd := exec.Command(dockerBinary, "hosts")
 	out, _, err = runCommandWithOutput(hostsCmd)
-	errorOut(err, t, fmt.Sprintf("listing hosts failed with errors: %v", err))
+	if err != nil {
+		t.Fatal(out, err)
+	}
+	if !strings.Contains(out, "test") {
+		t.Fatal("hosts should've listed 'test'")
+	}
+	if !strings.Contains(out, "tcp://10.11.12.13:2375") {
+		t.Fatal("hosts should've listed tcp://10.11.12.13:2375")
+	}
 
+	lsCmd := exec.Command(dockerBinary, "hosts", "ls")
+	out, _, err = runCommandWithOutput(lsCmd)
+	if err != nil {
+		t.Fatal(out, err)
+	}
 	if !strings.Contains(out, "test") {
 		t.Fatal("hosts should've listed 'test'")
 	}
 
+	urlCmd := exec.Command(dockerBinary, "hosts", "url", "test")
+	out, _, err = runCommandWithOutput(urlCmd)
+	if err != nil {
+		t.Fatal(out, err)
+	}
+	if !strings.Contains(out, "tcp://10.11.12.13:2375") {
+		t.Fatal("hosts should've listed tcp://10.11.12.13:2375")
+	}
+
+	inspectCmd := exec.Command(dockerBinary, "hosts", "inspect", "test")
+	out, _, err = runCommandWithOutput(inspectCmd)
+	if err != nil {
+		t.Fatal(out, err)
+	}
 	if !strings.Contains(out, "tcp://10.11.12.13:2375") {
 		t.Fatal("hosts should've listed tcp://10.11.12.13:2375")
 	}
@@ -33,11 +70,144 @@ func TestHostsEnsureHostIsListed(t *testing.T) {
 	logDone("hosts - host is created")
 }
 
-func TestHostsEnsureHostConnects(t *testing.T) {
+func TestHostsEnsureConnectsToActiveHost(t *testing.T) {
+	if err := clearHosts(); err != nil {
+		t.Fatal(err)
+	}
+
+	err := assertConnectionIsMadeToServer(func(addr string) error {
+		// Create host which points at server
+		createCmd := exec.Command(dockerBinary,
+			"hosts",
+			"create",
+			"--url", addr,
+			"test")
+		_, _, err := runCommandWithOutput(createCmd)
+		if err != nil {
+			return err
+		}
+
+		// Run command to connect to host
+		psCmd := exec.Command(dockerBinary, "ps")
+		_, _, err = runCommandWithOutput(psCmd)
+		return err
+	})
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	logDone("hosts - connects to active host")
+}
+
+func TestHostsEnsureOptionWithNameOverridesActiveHost(t *testing.T) {
+	if err := clearHosts(); err != nil {
+		t.Fatal(err)
+	}
+
+	err := assertConnectionIsMadeToServer(func(addr string) error {
+		// Create host which points at server
+		createCmd := exec.Command(dockerBinary,
+			"hosts",
+			"create",
+			"--url", addr,
+			"test")
+		_, _, err := runCommandWithOutput(createCmd)
+		if err != nil {
+			return err
+		}
+		activeCmd := exec.Command(dockerBinary, "hosts", "active", "default")
+		_, _, err = runCommandWithOutput(activeCmd)
+		if err != nil {
+			return err
+		}
+
+		// Run command to connect to host
+		psCmd := exec.Command(dockerBinary, "-H", "test", "ps")
+		_, _, err = runCommandWithOutput(psCmd)
+		return err
+	})
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	logDone("hosts - host option with name overrides active host")
+}
+
+func TestHostsEnsureOptionWithURLOverridesActiveHost(t *testing.T) {
+	if err := clearHosts(); err != nil {
+		t.Fatal(err)
+	}
+
+	err := assertConnectionIsMadeToServer(func(addr string) error {
+		// Create host which points at server
+		createCmd := exec.Command(dockerBinary,
+			"hosts",
+			"create",
+			"--url", "unix://another/url",
+			"test")
+		_, _, err := runCommandWithOutput(createCmd)
+		if err != nil {
+			return err
+		}
+
+		// Run command to connect to host
+		psCmd := exec.Command(dockerBinary, "-H", addr, "ps")
+		_, _, err = runCommandWithOutput(psCmd)
+		return err
+	})
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	logDone("hosts - host option with URL overrides active host")
+
+}
+
+func TestHostsEnsureHostIsRemoved(t *testing.T) {
+	if err := clearHosts(); err != nil {
+		t.Fatal(err)
+	}
+
+	createCmd := exec.Command(dockerBinary,
+		"hosts",
+		"create",
+		"--url", "tcp://10.11.12.13:2375",
+		"test")
+	out, _, err := runCommandWithOutput(createCmd)
+	if err != nil {
+		t.Fatal(out, err)
+	}
+
+	out, _, err = runCommandWithOutput(exec.Command(dockerBinary, "hosts"))
+	if err != nil {
+		t.Fatal(out, err)
+	}
+
+	if !strings.Contains(out, "test") {
+		t.Fatal("hosts should've listed 'test'")
+	}
+
+	out, _, err = runCommandWithOutput(exec.Command(dockerBinary,
+		"hosts", "rm", "test",
+	))
+	if err != nil {
+		t.Fatal(out, err)
+	}
+
+	if strings.Contains(out, "test") {
+		t.Fatal("hosts should not list 'test'")
+	}
+
+	logDone("hosts - host is removed")
+}
+
+// assertConnectionIsMadeToServer creates a server to listen on and ensures
+// that the function inside makes a connection to that server.
+func assertConnectionIsMadeToServer(f func(addr string) error) error {
 	// Set up server to listen on
 	ln, err := net.Listen("tcp4", "127.0.0.1:0")
 	if err != nil {
-		t.Fatalf("Listen failed: %v", err)
+		return fmt.Errorf("Listen failed: %v", err)
 	}
 	defer ln.Close()
 	ch := make(chan error, 1)
@@ -51,61 +221,24 @@ func TestHostsEnsureHostConnects(t *testing.T) {
 		ch <- nil
 	}()
 
-	// Create host which points at server
-	createCmd := exec.Command(dockerBinary,
-		"hosts",
-		"create",
-		"-o", "url=tcp://"+ln.Addr().String(),
-		"test")
-	_, _, err = runCommandWithOutput(createCmd)
-	errorOut(err, t, fmt.Sprintf("creating host failed with errors: %v", err))
-
-	// Run command to connect to host
-	psCmd := exec.Command(dockerBinary, "-H", "test", "ps")
-	_, _, err = runCommandWithOutput(psCmd)
-
-	timeout := make(chan bool, 1)
-	go func() {
-		time.Sleep(1 * time.Second)
-		timeout <- true
-	}()
+	err = f(fmt.Sprintf("tcp://%s", ln.Addr().String()))
 
 	select {
 	case err = <-ch:
 		if err != nil {
-			t.Error(err)
+			return err
 		}
-	case <-timeout:
-		t.Fatal("no connection was made to server")
+	case <-time.After(5 * time.Second):
+		return fmt.Errorf("no connection was made to server")
 	}
 
-	logDone("hosts - host connects to server")
+	return nil
 }
 
-func TestHostsEnsureHostIsRemoved(t *testing.T) {
-	createCmd := exec.Command(dockerBinary,
-		"hosts",
-		"create",
-		"-o", "url=tcp://10.11.12.13:2375",
-		"test")
-	out, _, err := runCommandWithOutput(createCmd)
-	errorOut(err, t, fmt.Sprintf("creating host failed with errors: %v", err))
-
-	out, _, err = runCommandWithOutput(exec.Command(dockerBinary, "hosts"))
-	errorOut(err, t, fmt.Sprintf("listing hosts failed with errors: %v", err))
-
-	if !strings.Contains(out, "test") {
-		t.Fatal("hosts should've listed 'test'")
+func clearHosts() error {
+	homeDir, err := homedir.Dir()
+	if err != nil {
+		return err
 	}
-
-	out, _, err = runCommandWithOutput(exec.Command(dockerBinary,
-		"hosts", "rm", "test",
-	))
-	errorOut(err, t, fmt.Sprintf("creating host failed with errors: %v", err))
-
-	if strings.Contains(out, "test") {
-		t.Fatal("hosts should not list 'test'")
-	}
-
-	logDone("hosts - host is removed")
+	return os.RemoveAll(path.Join(homeDir, ".docker/hosts"))
 }
