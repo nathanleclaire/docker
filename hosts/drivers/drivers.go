d3f0bb5b33ce57f8f32badc126d52774061a4728
Ben Firshman <ben@firshman.co.uk>
2014-11-10 12:28:26 +0000

    Document driver interface

--- hosts/drivers/drivers.go
+++ hosts/drivers/drivers.go
@@ -14,21 +14,58 @@ import (
 // driver represent different ways hosts can be created (e.g. different
 // hypervisors, different cloud providers)
 type Driver interface {
+	// DriverName returns the name of the driver as it is registered
 	DriverName() string
-	GetURL() (string, error)
-	GetIP() (string, error)
-	GetState() (state.State, error)
-	Create() error
+
+	// SetConfigFromFlags configures the driver with the object that was returned
+	// by RegisterCreateFlags
 	SetConfigFromFlags(flags interface{}) error
+
+	// GetURL returns a Docker compatible host URL for connecting to this host
+	// e.g. tcp://1.2.3.4:2376
+	GetURL() (string, error)
+
+	// GetIP returns an IP or hostname that this host is available at
+	// e.g. 1.2.3.4 or docker-host-d60b70a14d3a.cloudapp.net
+	GetIP() (string, error)
+
+	// GetState returns the state that the host is in (running, stopped, etc)
+	GetState() (state.State, error)
+
+	// Create a host using the driver's config
+	Create() error
+
+	// Remove a host
 	Remove() error
+
+	// Start a host
 	Start() error
+
+	// Stop a host gracefully
 	Stop() error
+
+	// Restart a host. This may just call Stop(); Start() if the provider does not
+	// have any special restart behaviour.
 	Restart() error
+
+	// Kill stops a host forcefully
 	Kill() error
+
+	// Upgrade the version of Docker on the host to the latest version
 	Upgrade() error
+
+	// GetSSHCommand returns a command for SSH pointing at the correct user, host
+	// and keys for the host with args appended. If no args are passed, it will
+	// initiate an interactive SSH session as if SSH were passed no args.
 	GetSSHCommand(args ...string) (*exec.Cmd, error)
 }
 
+// RegisteredDriver is used to register a driver with the Register function.
+// It has two attributes:
+// - New: a function that returns a new driver given a path to store host
+//   configuration in
+// - RegisterCreateFlags: a function that takes the FlagSet for
+//   "docker hosts create" and returns an object to pass to SetConfigFromFlags
 type RegisteredDriver struct {
 	New                 func(storePath string) (Driver, error)
 	RegisterCreateFlags func(cmd *flag.FlagSet) interface{}
@@ -44,6 +81,7 @@ func init() {
 	drivers = make(map[string]*RegisteredDriver)
 }
 
+// Register a driver
 func Register(name string, registeredDriver *RegisteredDriver) error {
 	if _, exists := drivers[name]; exists {
 		return fmt.Errorf("Name already registered %s", name)
@@ -62,9 +100,8 @@ func NewDriver(name string, storePath string) (Driver, error) {
 	return driver.New(storePath)
 }
 
-// RegisterCreateFlags registers the flags for all of the drivers but ignores
-// the value of the flags. A second pass is done to gather the value of the
-// flags once we know what driver has been picked
+// RegisterCreateFlags runs RegisterCreateFlags for all of the drivers and
+// returns their return values indexed by the driver name
 func RegisterCreateFlags(cmd *flag.FlagSet) map[string]interface{} {
 	flags := make(map[string]interface{})
 	for driverName := range drivers {
